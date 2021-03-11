3bfad1b8e7df83b693bd36beac7b41968e858d4e
Chikachi <Chikachi@users.noreply.github.com>
2021-03-11 03:29:46 +0100

    Update low_scorer.lua

--- low_scorer.lua
+++ low_scorer.lua
@@ -1,14 +1,18 @@
 meta.name = "Low Score \"Helper\""
-meta.version = "1.0.0"
+meta.version = "1.1.0"
 meta.description = "Pick up money and die."
 meta.author = "MiraWaNeko"
 
 money_time = nil
 spawning_bombs = false
 
+register_option_bool("instant_kill", "Instantly kill player when picking up money", false)
+
 function on_frame()
   if players[1].health > 0 and players[1].inventory.money > 0 then
-    if money_time == nil then
+    if options.instant_kill then
+      kill_entity(players[1].uid)
+    elseif money_time == nil then
       money_time = state.time_total
       toast("Oopsie Woopsie! I made a fucky wucky! A wittle fucko boingo!")
     else
