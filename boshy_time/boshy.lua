a8212e22ee9c33e0c9a8226b6b8590889c3031c6
Chikachi <chikachi@chikachi.net>
2021-03-25 17:06:07 +0100

    Added volume option

--- boshy_time/boshy.lua
+++ boshy_time/boshy.lua
@@ -1,12 +1,18 @@
 meta.name = "It's Boshy Time"
-meta.version = "1.0.0"
+meta.version = "1.0.1"
 meta.description = "Plays \"It's Boshy Time\" on run start"
 meta.author = "MiraWaNeko"
 
 boshy_sound = create_sound('boshy.mp3')
 
+register_option_int("volume", "Volume of sound file", 100, 0, 100)
+
 function on_start()
   if boshy_sound ~= nil then
-    boshy_sound:play()
+    player = boshy_sound:play(true)
+    player:set_volume(options.volume / 100.0)
+    player:set_pause(false)
+  else
+    message('Boshy sound was not found...')
   end
 end
